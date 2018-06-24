db9150cf05456d51c62ef7494a3f77ba0b6f4d16
Carl Eiserman <carleiserman@gmail.com>
2018-06-24 22:00:41 +0200

    chore: configuration milestone

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,21 @@
 const Configuration = {
-    /*
+  /*
      * Resolve and load @commitlint/config-conventional from node_modules.
      * Referenced packages must be installed
      */
-    extends: ['@commitlint/config-conventional'],
-    /*
+  extends: ['@commitlint/config-conventional'],
+  /*
      * Resolve and load conventional-changelog-atom from node_modules.
      * Referenced packages must be installed
      */
-    parserPreset: 'conventional-changelog-atom',
-    /*
+  parserPreset: 'conventional-changelog-atom',
+  /*
      * Any rules defined here will override rules from @commitlint/config-conventional
      */
-    rules: {
-        'type-enum': [2, 'always', ['chore', 'feat', 'fix', 'none']],
-        'subject-case': [2, 'always', ['lower-case']]
-    }
+  rules: {
+    'type-enum': [2, 'always', ['chore', 'feat', 'fix', 'none']],
+    'subject-case': [2, 'always', ['lower-case']],
+  },
 };
 
 module.exports = Configuration;
--- src/core/app/App.tsx
+++ src/core/app/App.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import './App.css';
 
-import logoSvg from '~/resources/logo.svg';
+import mainlogo from '~/resources/mainlogo.svg';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logoSvg} className="App-logo" alt="logo" />
+          <img src={mainlogo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
