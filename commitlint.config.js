4d51fc94ace1c4d47eec087edf651cf99da4a866
Carl Eiserman <carleiserman@gmail.com>
2018-06-24 20:50:50 +0200

    chore: set up commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+const Configuration = {
+    /*
+     * Resolve and load @commitlint/config-conventional from node_modules.
+     * Referenced packages must be installed
+     */
+    extends: ['@commitlint/config-conventional'],
+    /*
+     * Resolve and load conventional-changelog-atom from node_modules.
+     * Referenced packages must be installed
+     */
+    parserPreset: 'conventional-changelog-atom',
+    /*
+     * Any rules defined here will override rules from @commitlint/config-conventional
+     */
+    rules: {
+        'type-enum': [2, 'always', ['chore', 'feat', 'fix', 'none']]
+    }
+};
+
+module.exports = Configuration;
